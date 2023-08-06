ec6379deb525e1fd8290615cbfbb98bf792541a4
Alessandro Nikolaev <alessandronikolaev.business@gmail.com>
2023-08-06 01:06:01 +0100

    Added osu! wacom tablet settings

--- .sanyok/wacom.sh
+++ .sanyok/wacom.sh
@@ -2,7 +2,12 @@
 
 # Id 8: "Wacom Intuos BT S Pen stylus"
 # Id 9: "Wacom Intuos BT S Pad pad"
-xsetwacom set 8 Area 0 0 15200 8550
+
+# Art
+# xsetwacom set 8 Area 0 0 15200 8550 
+# osu!
+xsetwacom set 8 Area 0 0 9120 5130
+
 xsetwacom set 9 Button 1 key "+Ctrl S"
 xsetwacom set 9 Button 2 key "button 3"
 xsetwacom set 9 Button 3 key "+Ctrl +Shift Z"
