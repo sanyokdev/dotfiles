b14a1683ea86e891d95483281df0f62083b7fb0b
Alessandro Nikolaev <alessandronikolaev.business@gmail.com>
2023-08-09 01:53:25 +0100

    Removed awesome & correctly setup bspwm + others

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,13 +1,14 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
-# killall -q polybar
+# If all your bars have IPC enabled, you can use polybar-msg cmd quit
+# Otherwise, you can use the nuclear option: killall -q polybar
 
-# Launch polybar
-echo "---" | tee -a /tmp/polybar1.log
-polybar ikigai 2>&1 | tee -a /tmp/polybar1.log & disown
+# Launch Polybar and log the output to a file
+{
+  polybar-msg cmd quit  # Attempt to gracefully quit running instances
+  killall -q polybar   # If graceful quit fails, forcefully terminate instances
 
-echo "Bars launched..."
+  echo "--- Polybar Launched!"
+  polybar ikigai  # Launch bar
+} > /tmp/polybar1.log 2>&1 & disown
