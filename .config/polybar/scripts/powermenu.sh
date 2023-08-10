92501844bcb1e779cbfab170437a25bee6347bd6
Alessandro Nikolaev <alessandronikolaev.business@gmail.com>
2023-08-10 01:24:35 +0100

    Riced bspwm, polybar, sxhkd & .xinitrc

---
+++ .config/polybar/scripts/powermenu.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+choice=$(printf " Log Out\n Power Off\n Restart" | rofi -dmenu -i -p "Power option:")
+case "$choice" in
+    " Log Out") bspc quit ;;
+    " Power Off") systemctl poweroff ;;
+    " Restart") systemctl reboot ;;
+    *) exit 1 ;;
+esac
