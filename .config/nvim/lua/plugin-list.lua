b2a573d4e4f9e33398a22adf1f7636b9b9be4d13
A. Dominik <alessandronikolaev.business@gmail.com>
2024-04-20 00:48:43 +0100

    Updated nvim plugins

--- .config/nvim/lua/plugin-list.lua
+++ .config/nvim/lua/plugin-list.lua
@@ -146,7 +146,7 @@ require("lazy").setup({
 			"hrsh7th/cmp-cmdline", -- source for vim cmd
 
 			-- Snippets
-			{ "L3MON4D3/LuaSnip", version = "2.2.0" }, -- snippet engine
+			{ "L3MON4D3/LuaSnip", version = "2.3.0" }, -- snippet engine
 			"saadparwaiz1/cmp_luasnip", -- source for autocomplete
 		},
 	},
